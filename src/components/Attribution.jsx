a343f71c334b9da2fdd834e5b046fe792a09f02e
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-11-30 13:01:02 +0100

    Fix footer design.

---
+++ src/components/Attribution.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Attribution() {
+    return (
+        <div class="attribution">
+            Challenge by
+            <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"> Frontend Mentor</a>. Coded by
+            <a href="https://github.com/srglbrkchn"> srglbrkchn</a>.
+        </div>
+    );
+}
+
+export default Attribution;
--- src/components/main-body/Content.jsx
+++ src/components/main-body/Content.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import Bottom from "./Bottom";
+import Attribution from "../Attribution";
 
 function Content() {
     return (
         <div className="content">
             <Header src="../../images/illustration-hero.svg" alt="Dancing girl"/>
             <Bottom />
+            <Attribution />
         </div>
     );
 }
